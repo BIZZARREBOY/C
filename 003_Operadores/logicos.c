e1ee8efd8309973b94d5fdb46c1abf65c9b057cd
BIZZARREBOY <luigod99@gmail.com>
2026-05-15 11:25:17 -0600

    Solucion de ejercicio

---
+++ 003_Operadores/logicos.c
@@ -0,0 +1,67 @@
+/*
+003 - Operadores (Logicos)
+
+    && (and) -> (a, b) -> (1 o 0)
+                    (1, 1) -> (1)
+                    (1, 0) -> (0)
+                    (0, 1) -> (0)
+                    (0, 0) -> (0)
+
+    || (or) -> (a, b) -> (1 o 0)
+                    (1, 1) -> (1)
+                    (1, 0) -> (1)
+                    (0, 1) -> (1)
+                    (0, 0) -> (0)
+    
+    ! (not) -> (a) -> (1 o 0)
+                    (1) -> (0)
+                    (0) -> (1)
+
+
+    3! = 6 
+*/
+
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+
+    printf("Ingrese los tres numeros");
+    scanf("%d%d%d", &a, &b, &c);
+
+    int mayor = (a >= b && a >= c) ? a : (b >= a && b >= c) ? b : c;
+    int menor = (a <= b && a <= c) ? a : (b <= a && b <= c) ? b : c;
+    int medio = (a != menor && a != mayor) ? a : (b != menor && b != mayor) ? b : c;
+
+    printf("%d<%d<%d", menor, medio, mayor);
+
+    /*if(a<b && a<c){
+            if(b<c){
+                printf("%d %d %d", a, b, c);
+            }else{
+                printf("%d %d %d", a, c, b);
+            }
+    }else if(b<a && b<c){
+        if(a<c){
+            printf("%d %d %d", b, a, c);
+        }else{
+            printf("%d %d %d", b, c, a);
+        }
+    } else {
+        if(a<b){
+            printf("%d %d %d", c, a, b);
+        }else{
+            printf("%d %d %d", c, b, a);
+        }
+    }*/
+        
+    return 0;
+}
+
+
+// a<b<c
+// a<c<b
+// b<a<c
+// b<c<a
+// c<a<b
+// c<b<a
